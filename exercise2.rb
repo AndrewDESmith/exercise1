9ffaff90cf19b40b0ae9168568431c1bee79404c
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-06-03 12:29:25 -0400

    Exercise 3

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+tip = 55 * 0.20
+puts "Calculated 20% tip is #{tip}."
+
+puts "string" + 5.to_s
+
+result = 45628 * 7839
+puts "Interpolated multipled result of 45628 * 7839 is #{result}."
+
+puts (true && false) || (false && true) || !(false && false)
